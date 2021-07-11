dfda79d85af67cf637671b08712a079fa64c2ce9
landonbuell <lhb1007@wildcats.unh.edu>
2021-07-10 21:33:01 -0400

    Updates to Python Project

--- FloatingPointPrecision/Perceptrons-Py/SingleLayerPerceptron.py
+++ FloatingPointPrecision/Perceptrons-Py/SingleLayerPerceptron.py
@@ -35,6 +35,8 @@ class SingleLayerPerceptron:
 		self._activations = np.empty(
 			shape=(self._nClasses,self._batchSize),dtype=self._dType)
 
+		self._objectiveFunction = None
+
 	def __del__(self):
 		""" Constructor for SingleLayerPerceptronInstance """
 		pass
@@ -43,6 +45,7 @@ class SingleLayerPerceptron:
 
 	def Train(self,X,Y):
 		""" Train Model W/ Inputs X + Y """
+		self.call(X)		# execute forward pass
 
 
 	""" Getters and Setters """
@@ -96,14 +99,65 @@ class SingleLayerPerceptron:
 		""" Get the shape of Weights Array """
 		return (self._nClasses,)
 
+	def getObjective(self):
+		""" Get the Objective Functions for this Model """
+		return self._objectiveFunction
+
+	def setObjective(self,x):
+		""" Set the Objective Function for this Model """
+		self._objectiveFunction = x
+		return self
+
 	""" Private Interface """
 
-	def validateShape(shapeExpt,X):
+	def validateShape(self,x):
 		""" Test if given shape is the same as expected """
-		pass
-
+		if (x.ndim != 2):
+			raise RuntimeError("Array must be 2D!")
+		elif (x.shape[1] != self._nFeatures):
+			raise Exception("Array Shape mismatch!")
+		else:
+			self.setBatchSize(x.shape[0])
+			return True
+			
 	def call(self,X):
 		""" Execute Forward Pass """
-		self._
-		self._activations = np.matmul(X,self._weights) + self._biases
+		self.validateShape(X)
+		np.matmul(X,self._weights,out=self._activations) + self._biases
 		return self
+
+class ObjectiveFunctions:
+	""" Static Class of Objective Functions """
+
+	def __init__(self):
+		""" Constructor For MeanSquaredErrorObjective Instance """
+		raise Exception("Static Class - Make No Instance")
+
+	def __del__(self):
+		""" Constructor For MeanSquaredErrorObjective Instance """
+		pass
+
+	@staticmethod
+	def compareShapes(shapeX,shapeY):
+		""" Comapre Shape of Input to Output """
+		if (shapeX != shapeY):
+			raise Exception("Shape Mismatch!")
+		else:
+			return True
+
+	@staticmethod
+	def meanSquaredError(X,Y):
+		""" Compute Squared Error of X & Y """	
+		nSamples = X.shape[0]
+		mse = (1/nSamples) * (X - Y)**2 
+		return mse
+
+	@staticmethod
+	def categoricalCrossEntropy(X,Y):
+		nSamples = X.shape[0]
+		nClasses = X.shape[1]
+		raise NotImplementedError()
+		
+
+
+		
